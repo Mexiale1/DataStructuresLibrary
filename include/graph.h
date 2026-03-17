1c4e58a4b4ea4c1b346367bb007201bf4a2f6f4b
Mexiale1 <cheedos123@gmail.com>
2026-03-17 20:01:07 +0800

    Added deque and graph ADT

--- include/graph.h
+++ include/graph.h
@@ -1,1 +1,11 @@
-yee haw
+#pragma once
+
+#include "list.h"
+
+template <typename T> struct Graph {
+  virtual bool addEdge(const int &i, const int &j) = 0;
+  virtual bool removeEdge(const int &i, const int &j) = 0;
+  virtual bool hasEdge(const int &i, const int &j) const = 0;
+  virtual List<T> outEdges(const int &i) const = 0;
+  virtual List<T> inEdges(const int &i) const = 0;
+};
