dc6a6448ff9780ed8611e5020a5df00612a3a776
Mexiale1 <cheedos123@gmail.com>
2026-03-17 15:35:45 +0800

    I hate ryan

--- include/queue.h
+++ include/queue.h
@@ -1,1 +1,7 @@
-a
+#pragma once
+
+template <typename T> struct Queue {
+  virtual void add(const T &x) = 0;
+  virtual T remove() = 0;
+  virtual bool empty() const = 0;
+};
--- include/sset.h
+++ include/sset.h
@@ -1,1 +1,11 @@
-a
+#pragma once
+
+#include <stddef.h>
+
+template <typename T> struct SortedSet {
+  virtual size_t size() const = 0;
+  virtual bool add(const T &x) = 0;
+  virtual T remove(const T &x) = 0;
+  virtual T find(const T &x) const = 0;
+  virtual void compare(T &x, T &y) = 0;
+};
