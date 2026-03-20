f2ad4d9e0610ae9d4f1d0d3a78b9843c52393432
Mexiale1 <cheedos123@gmail.com>
2026-03-20 10:26:47 +0800

    Added array.h something idk kinda useful

--- include/array.h
+++ include/array.h
@@ -1,8 +1,57 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdexcept>
 
 template <typename T> struct array {
-  T *x;
+  T *arr;
   size_t length;
+
+  array(size_t n) {
+    arr = new T[n];
+    length = n;
+  }
+
+  array(array<T> &anotherArr) {
+    length = anotherArr.length;
+    arr = new T[length];
+
+    for (size_t i = 0; i < length; i++) {
+      arr[i] = anotherArr.arr[i];
+    }
+  }
+
+  array(array<T> &&anotherTempArr) {
+    arr = anotherTempArr.arr;
+    length = anotherTempArr.length;
+    anotherTempArr.arr = nullptr;
+  }
+
+  ~array() { delete[] arr; }
+
+  T &operator[](size_t index) {
+    if (index >= length) {
+      throw std::out_of_range("Out of bounds\n");
+    }
+
+    return arr[index];
+  }
+
+  array<T> &operator=(array<T> &anotherArr) {
+    if (this == &anotherArr) {
+      return *this;
+    }
+
+    if (arr != nullptr) {
+      delete[] arr;
+    }
+
+    arr = anotherArr.arr;
+    length = anotherArr.length;
+    return *this;
+  }
+
+  T begin() { return arr; }
+
+  T end() { return arr + length; }
 };
