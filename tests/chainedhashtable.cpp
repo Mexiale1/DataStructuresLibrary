5e74741de8fb29ac0e589d2f6359ebe9d2f53be5
Mexiale1 <cheedos123@gmail.com>
2026-03-22 01:46:02 +0800

    im so bad

--- tests/chainedhashtable.cpp
+++ tests/chainedhashtable.cpp
@@ -1,4 +1,4 @@
-#define UPPER_LIMIT 10000000
+#define UPPER_LIMIT 1000000
 
 #include "../src/chainedhashtable.h"
 #include <chrono>
@@ -6,36 +6,65 @@
 
 int main() {
   ChainedHashTable<int> dog;
+  auto minAdd = std::chrono::duration<double>::max();
+  auto maxAdd = std::chrono::duration<double>::zero();
 
-  std::chrono::time_point beforeTest = std::chrono::system_clock::now();
+  auto minFind = std::chrono::duration<double>::max();
+  auto maxFind = std::chrono::duration<double>::zero();
 
-  for (size_t i = 0; i < UPPER_LIMIT; i++) {
-    int randValue = std::rand();
-    dog.add(randValue);
+  auto avgAdd = std::chrono::duration<double>::zero();
+  auto avgFind = std::chrono::duration<double>::zero();
+
+  size_t i = 0;
+
+  while (i < 10) {
+    std::chrono::time_point beforeTest = std::chrono::system_clock::now();
+
+    for (size_t i = 0; i < UPPER_LIMIT; i++) {
+      int randValue = std::rand();
+      dog.add(randValue);
+    }
+
+    std::chrono::time_point afterAdd = std::chrono::system_clock::now();
+
+    for (size_t i = 0; i < UPPER_LIMIT; i++) {
+      int randValue = std::rand();
+      dog.find(randValue);
+    }
+    std::chrono::time_point afterFind = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> add = afterAdd - beforeTest;
+    std::chrono::duration<double> find = afterFind - afterAdd;
+
+    std::cout << "Add: " << add.count() << "s" << std::endl;
+    std::cout << "Find: " << find.count() << "s" << std::endl;
+
+    if (add < minAdd)
+      minAdd = add;
+    if (add > maxAdd)
+      maxAdd = add;
+
+    if (find < minFind)
+      minFind = find;
+    if (find > maxFind)
+      maxFind = find;
+
+    avgAdd += add;
+    avgFind += find;
+    i++;
   }
 
-  std::chrono::time_point afterAdd = std::chrono::system_clock::now();
+  avgFind /= 10;
+  avgAdd /= 10;
 
-  for (size_t i = 0; i < UPPER_LIMIT; i++) {
-    int randValue = std::rand();
-    dog.find(randValue);
-  }
+  std::cout << "Minimum time (add): " << minAdd.count() << "s" << std::endl;
+  std::cout << "Maximum time (add): " << maxAdd.count() << "s" << std::endl;
 
-  std::chrono::time_point afterFind = std::chrono::system_clock::now();
+  std::cout << "Minimum time (find): " << minAdd.count() << "s" << std::endl;
+  std::cout << "Maximum time (find): " << maxAdd.count() << "s" << std::endl;
 
-  for (size_t i = 0; i < UPPER_LIMIT / 10; i++) {
-    dog.remove(0);
-  }
-
-  std::chrono::time_point afterRemove = std::chrono::system_clock::now();
-
-  std::chrono::duration<double> add = afterAdd - beforeTest;
-  std::chrono::duration<double> sorting = afterFind - afterAdd;
-  std::chrono::duration<double> remove = afterRemove - afterFind;
-
-  std::cout << "Add: " << add.count() << "s" << std::endl;
-  std::cout << "Find: " << sorting.count() << "s" << std::endl;
-  std::cout << "Remove: " << remove.count() << "s" << std::endl;
+  std::cout << "Average time (add): " << avgAdd.count() << "s" << std::endl;
+  std::cout << "Average time (find): " << avgFind.count() << "s" << std::endl;
 
   return 0;
 }
