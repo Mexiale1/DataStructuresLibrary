77a908303a51fe44f73d898c6ba5ae82075c8590
Mexiale1 <cheedos123@gmail.com>
2026-03-21 22:07:24 +0800

    Done benchmarking for DLList

--- tests/dllist.cpp
+++ tests/dllist.cpp
@@ -1,22 +1,38 @@
+#define UPPER_LIMIT 10000000
+
 #include "../src/dllist.h"
+#include <chrono>
+#include <cstdlib>
 
 int main() {
   DLList<int> dog; // Testing if it has no errors
 
-  for (size_t i = 0; i < 100; i++) {
-    dog.addFirst(i);
+  std::chrono::time_point beforeTest = std::chrono::system_clock::now();
+
+  for (size_t i = 0; i < UPPER_LIMIT; i++) {
+    int randValue = std::rand();
+    dog.addFirst(randValue);
   }
 
-  dog.printList();
+  std::chrono::time_point afterAdd = std::chrono::system_clock::now();
 
-  std::cout << "Remove test\n";
+  dog.myMergeSort();
 
-  for (size_t i = 0; i < 90; i++) {
-    std::cout << i << ": " << dog.size() << std::endl;
+  std::chrono::time_point afterSort = std::chrono::system_clock::now();
+
+  for (size_t i = 0; i < UPPER_LIMIT / 10; i++) {
     dog.remove(0);
   }
 
-  dog.printList();
+  std::chrono::time_point afterRemove = std::chrono::system_clock::now();
+
+  std::chrono::duration<double> add = afterAdd - beforeTest;
+  std::chrono::duration<double> sorting = afterSort - afterAdd;
+  std::chrono::duration<double> remove = afterRemove - afterSort;
+
+  std::cout << "Add: " << add.count() << "s" << std::endl;
+  std::cout << "Sorting: " << sorting.count() << "s" << std::endl;
+  std::cout << "Remove: " << remove.count() << "s" << std::endl;
 
   return 0;
 }
