14036dc989564e326138dee7479398a957737357
PiyaaYUH <feah.bentazal06@gmail.com>
2026-03-21 23:45:57 +0800

    Created benchmark folder

---
+++ benchmarks/bm_sllist.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include "../src/sllist.h"
+
+using namespace std;
+
+const size_t UPPER_LIMIT = 1000000; // adjust size for benchmarking
+
+int main() {
+    SLList<int> q;
+
+    auto startAdd = chrono::high_resolution_clock::now();
+
+    for (size_t i = 0; i < UPPER_LIMIT; i++) {
+        int val = rand();
+        q.add(val);
+    }
+
+    auto endAdd = chrono::high_resolution_clock::now();
+
+    auto startRemove = chrono::high_resolution_clock::now();
+
+
+     while (!q.empty()) {
+            q.remove();
+    }
+
+    auto endRemove = chrono::high_resolution_clock::now();
+
+    chrono::duration<double> timeAdd = endAdd - startAdd;
+    chrono::duration<double> timeRemove = endRemove - startRemove;
+
+    cout << "Time for add() (FIFO):      " << timeAdd.count() << "s\n";
+    cout << "Time for remove():          " << timeRemove.count() << "s\n";
+
+    return 0;
+}
